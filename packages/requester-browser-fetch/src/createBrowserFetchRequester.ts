a64d48a77cac60a8e48bd35948e6440932fe389e
gsm-josh <joshua@greyskymedia.com>
2021-01-04 07:21:56 -0600

    changed fetch async structure

--- packages/requester-browser-fetch/src/createBrowserFetchRequester.ts
+++ packages/requester-browser-fetch/src/createBrowserFetchRequester.ts
@@ -2,30 +2,30 @@ import { Request, Requester, Response } from '@algolia/requester-common';
 
 export function createBrowserFetchRequester(): Requester {
   return {
-    send(request: Request): Readonly<Promise<Response>> {
+    async send(request: Request): Promise<Response> {
         let request_params: RequestInit = {
             headers: request.headers,
             method: request.method
         };
-
+        console.log(request.method);
+        console.log(request.url);
         let req = new Request(request.url,request_params);
 
-        return fetch(req)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(response.statusText)
-                }
+        let response = await fetch(req);
 
-                return new Promise( (resolve): void => {
-                    response.text().then(text => {
-                        return {
-                            content: response.text(),
-                            status: response.status,
-                            isTimedOut: false,
-                        }
-                    });
-                });
-            }) as Promise<Response>;
+        if (!response.ok) {
+            throw new Error(response.statusText)
+        }
+
+        return new Promise( (resolve): void => {
+            response.text().then(text => {
+                return {
+                    content: response.text(),
+                    status: response.status,
+                    isTimedOut: false,
+                }
+            });
+        });
     },
   };
 }
