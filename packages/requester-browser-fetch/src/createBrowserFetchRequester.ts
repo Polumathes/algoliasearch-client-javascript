85f3c537e9872cbc8f993a5ac30d425e400541d8
gsm-josh <joshua@greyskymedia.com>
2021-01-04 09:45:31 -0600

    completed fetch client

--- packages/requester-browser-fetch/src/createBrowserFetchRequester.ts
+++ packages/requester-browser-fetch/src/createBrowserFetchRequester.ts
@@ -3,29 +3,21 @@ import { Request, Requester, Response } from '@algolia/requester-common';
 export function createBrowserFetchRequester(): Requester {
   return {
     async send(request: Request): Promise<Response> {
-        let request_params: RequestInit = {
-            headers: request.headers,
-            method: request.method
-        };
-        console.log(request.method);
-        console.log(request.url);
-        let req = new Request(request.url,request_params);
+        request.body = request.data;
+
+        let req = new Request(request.url, request);
 
         let response = await fetch(req);
-
         if (!response.ok) {
-            throw new Error(response.statusText)
+            throw new Error(response.statusText);
         }
 
-        return new Promise( (resolve): void => {
-            response.text().then(text => {
-                return {
-                    content: response.text(),
-                    status: response.status,
-                    isTimedOut: false,
-                }
-            });
-        });
+        let response_text = await response.text();
+        return {
+            content: response_text,
+            status: response.status,
+            isTimedOut: false,
+        };
     },
   };
 }
